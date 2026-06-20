53f78a0c6a580a6837516b252bdfbcdd5172a223
RafaelLfckkj <rafaxolive@gmail.com>
2026-06-20 16:20:07 -0300

    lembrei dos detalhes

---
+++ src/app/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white py-4 text-center">
+            <p>&copy; 2024 Testando e relembrando. Todos os direitos reservados.</p>
+        </footer>
+    )
+}
---
+++ src/app/contato/page.tsx
@@ -0,0 +1,7 @@
+export default function paginaContato() {
+    return(
+        <div>
+            <h1>Pagina de contato</h1>
+        </div>
+    )
+}
